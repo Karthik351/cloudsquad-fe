9e30b63e830fa7549c63bb1acaf2927b8bcff829
Karthik351 <152361790+Karthik351@users.noreply.github.com>
2025-03-31 14:04:15 -0400

    update api

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,7 @@
 export async function get_total_tests_taken(){
     try {
-        const response = await fetch ('https://aeb542xxee.execute-api.ap-south-1.amazonaws.com/prod/tests-taken');
+        //const response = await fetch ('https://aeb542xxee.execute-api.ap-south-1.amazonaws.com/prod/tests-taken');
+        const response = await fetch ('https://aeb542xxee.execute-api.ap-south-1.amazonaws.com/stage/tests-taken');
         const data = await response.json();
         const total_tests_taken = data.total_tests_taken;
         return total_tests_taken;
